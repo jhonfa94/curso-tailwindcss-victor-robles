7a683bf1aff8caa776309b26f1eea853f1a9239c
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2022-08-05 17:57:38 -0500

    Curso Taildwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,100 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+
+            screens: {
+                'xs': { 'max': '768px' }
+            },
+
+            backgroundImage: teme => ({
+                'body-pattern': "url('/img/pattern.png')",
+                'banner': "url('/img/bakbaner.png')",
+            }),
+            backgroundPosition: {
+                'banner-position': "-200px -200px"
+            },
+            colors: {
+                "azul-claro": "#37bcf9",
+                "azul-oscuro": "#0370b9",
+            },
+            animation: {
+                'spin-low': 'spin 2s linear infinite',
+                'from-bellow': 'fromBellow 500ms linear',
+                'from-right': 'fromRight 300ms linear',
+                'bg-banner': 'backBanner 10s linear',
+                'text-banner': 'showBannerText 10s linear',
+                'show-card-icon': 'showCardIcon 300ms linear',
+                'show-card-category': 'showCardCategory 400ms linear',
+                'show-card-description': 'showCardDescription 400ms linear',
+            },
+            keyframes: {
+                fromBellow: {
+                    '0%': { transform: 'translateY(0%)' },
+                    '50%': { transform: 'translateY(200%)' },
+                    '100%': { transform: 'translateY(0%)' }
+                },
+                fromRight: {
+                    '0%': { transform: 'translateX(200%)' },
+                    '100%': { transform: 'translateX(0%)' }
+                },
+                backBanner: {
+                    '0%': { 'background-position': '0px 0px' },
+                    '100%': { 'background-position': '-200px -200px' },
+                },
+                showBannerText: {
+                    '0%': {
+                        transform: 'translateX(-600%) scale(5,5)',
+                        opacity: 1
+                    },
+                    '50%': {
+                        transform: 'translateX(600%) scale(5,5)',
+                        opacity: 1
+                    },
+                    '75%': {
+                        transform: 'translateX(-600%) scale(5,5)',
+                        'text-shadow': 'none',
+                        color: 'transparent',
+                        opacity: 0
+                    },
+                    '100%': {
+                        transform: 'translateX(0%) scale(1,1)',
+                        'text-shadow': '2px 2px 1px black',
+                        color: 'white',
+                        opacity: 1
+                    },
+                },
+                showCardIcon: {
+                    'from': {
+                        transform: 'translateY(-200%)'
+                    },
+                    'to': {
+                        transform: 'translateY(0%)'
+                    },
+                },
+                showCardCategory: {
+                    'from': {
+                        transform: 'translateY(-300%)'
+                    },
+                    'to': {
+                        transform: 'translateY(0%)'
+                    },
+                },
+                showCardDescription: {
+                    'from': {
+                        transform: 'translateY(-300%)'
+                    },
+                    'to': {
+                        transform: 'translateY(0%)'
+                    },
+                }
+            }
+        },
+    },
+    plugins: [],
+}
